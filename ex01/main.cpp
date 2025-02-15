6530e73735c330f847612be3cd02916d51bebca1
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2025-02-15 21:27:47 +0300

    Add files via upload

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,18 +6,18 @@ int main()
     ds->a = 42;
     ds->b = ds->a * 1e-2;
 
-    cout << endl << "\033[0;36m-----------------------------------------------" << endl;
-    cout << "\033[0;32mBefore: \033[0m\033[0;36m" << ds << endl;
+    cout << endl << "-----------------------------------------------" << endl;
+    cout << "Before: " << ds << endl;
     uintptr_t rawPtr = Serializer::serialize(ds);
-    cout << "\033[0;31mAfter: \033[0m\033[0;36m" << rawPtr << endl;
+    cout << "After: " << rawPtr << endl;
     cout << "-----------------------------------------------" << endl << endl;
-    cout << "\033[0;37m-----------------------------------------------" << endl;
+    cout << "-----------------------------------------------" << endl;
 
     Data *tmp = NULL;
     tmp = Serializer::deserialize(rawPtr);
-    cout << "\033[0;34mBefore: \033[0m\033[0;37m" << tmp << endl;
-    cout << "\033[0;35mtmp->a: \033[0m\033[0;37m" << tmp->a << endl;
-    cout << "\033[0;35mtmp->b: \033[0m\033[0;37m" << tmp->b << endl;
+    cout << "Before: " << tmp << endl;
+    cout << "tmp->a: " << tmp->a << endl;
+    cout << "tmp->b: " << tmp->b << endl;
     cout << "-----------------------------------------------" << endl << endl;
 
     delete ds;
